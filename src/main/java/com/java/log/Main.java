793ee413e20ebb7ad61568eaf31f26f8178b9630
szhittech <szhittech>
2018-07-06 10:35:33 +0800

    com java log check

--- src/main/java/com/java/log/Main.java
+++ src/main/java/com/java/log/Main.java
@@ -3,17 +3,25 @@ package com.java.log;
 
 public class Main {
 
-    public static Thread testThread = null;
-
     public static void main(String[] args) {
+        //使用方法：
+        //1.指定端口，浏览器访问：http://IP:8126
         JavaLog.startLog(8126);
+
+        //2.指定端口和一个日志文件路径
+        //JavaLog.startLog(8126,"/home/java/gateway.log");
+
+        //3.关闭 JavaLog.stopLog();
+
+
+
+        //=========================测试，循环打印log=========================
         testThread = new Thread(new Runnable() {
             @Override
             public void run() {
-
                 try {
                     while (true) {
-                        System.out.println("what's up..");
+                        System.out.println("其傻屌..");
                         Thread.sleep(1111);
                     }
                 } catch (InterruptedException e) {
@@ -25,4 +33,5 @@ public class Main {
         });
         testThread.start();
     }
+    public static Thread testThread;
 }
---
+++ target/classes/webapp/js/websocket.js
@@ -0,0 +1,111 @@
+var ip;
+var port;
+function load() {
+    connect();
+}
+
+function connect() {
+    // var host = "ws://207.246.96.42:8125"
+    var host = "ws://"+ ip +":"+port;
+//    var host = "ws://192.168.1.105:8125"
+//     alert(host);
+    console.log("####websocket info " + host);
+    socket = new WebSocket(host);
+    try {
+        socket.onopen = function (msg) {
+            status("连接成功");
+        };
+        socket.onmessage = function (msg) {
+            if (typeof msg.data == "string") {
+                showLog(msg.data);
+            } else {
+                alert("非文本消息onmessage" + msg);
+            }
+        };
+
+        socket.onerror = function (msg) {
+            console.log('onerror received a message', msg);
+            status("连接失败");
+        };
+
+        socket.onclose = function (msg) {
+            console.log('onclose received a message', msg);
+            status("连接关闭");
+        };
+    } catch (ex) {
+        console.log('catch received a message', msg);
+    }
+}
+
+function send() {
+    var msg = getId("sendText").value
+    socket.send(msg);
+}
+
+function disconnect() {
+    try {
+        socket.close();
+        socket = null;
+    } catch (ex) {
+    }
+}
+
+function refresh() {
+    try {
+        socket.close();
+        socket = null;
+    } catch (ex) {
+    }
+    return "自定义内容";
+}
+
+window.onbeforeunload = function () {
+    try {
+        socket.close();
+        socket = null;
+    } catch (ex) {
+    }
+};
+
+function stop() {
+    try {
+        socket.close();
+        socket = null;
+    } catch (ex) {
+    }
+}
+
+function stopTest() {
+    var data = 'stoptest';
+    socket.send(data);
+}
+
+function getId(id) {
+    return document.getElementById(id);
+}
+
+
+function getText() {
+    return document.getElementById("txtContent").value
+}
+
+function status(status) {
+    getId("status").innerHTML = status;
+}
+
+function showLog(msg) {
+    var div = document.getElementById('txtContent');
+            div.value += "\r\n" + msg;
+            div.scrollTop = div.scrollHeight;
+}
+
+function clearLog() {
+    document.getElementById("txtContent").value = "";
+}
+
+
+function onkey(event) {
+    if (event.keyCode == 13) {
+        send();
+    }
+}
